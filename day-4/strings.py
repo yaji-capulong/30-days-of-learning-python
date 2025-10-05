c7ac9421701107c7e7c7be40f0f42e79b85f8d22
Mj Capulong <maryjay.capulonh@gmail.com>
2025-10-05 02:24:19 -0700

    day 4 start

---
+++ day-4/strings.py
@@ -0,0 +1,125 @@
+#Day 4: 30 Days of Python Programming
+
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+str1 = 'Thirty'
+str2 = 'Days'
+str3 = 'Of'
+str4 = 'Python'
+
+sentence = str1 + str2 + str3 + str4
+print(sentence)
+
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+first_word = 'Coding'
+second_word = 'For'
+third_word = 'All'
+
+sentence = str1 + str2 + str3
+print(sentence)
+
+#Declare a variable named company and assign it to an initial value "Coding For All".
+company = 'Coding For All'
+
+#Print the variable company using print().
+print(company)
+
+#Print the length of the company string using len() method and print().
+print(len(company))
+
+#Change all the characters to uppercase letters using upper() method.
+print(company.upper())
+
+#Change all the characters to lowercase letters using lower() method.
+print(company.lower())
+
+#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+#Cut(slice) out the first word of Coding For All string.
+coding = company[0:5]
+print(coding)
+
+#Check if Coding For All string contains a word Coding using the method index, find or other methods.
+print(company.index('coding'))
+print(company.find('coding'))
+
+#Replace the word coding in the string 'Coding For All' to Python.
+print(company.replace('coding', 'python'))
+
+#Change Python for Everyone to Python for All using the replace method or other methods.
+phrase =  'Python for Everyone'
+print(phrase.replace('Python', 'Coding'))
+
+#Split the string 'Coding For All' using space as the separator (split()) .
+company= 'Coding For All'
+print(company.split(' '))
+
+#"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+socmed = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+print(socmed.split(', '))
+
+#What is the character at index 0 in the string Coding For All.
+index = 0
+print(company[index])
+
+#What is the last index of the string Coding For All.
+lastletter = 'l'
+print(company.rindex(lastletter))
+
+#What character is at index 10 in "Coding For All" string.
+print(company[10])
+
+#Create an acronym or an abbreviation for the name 'Python For Everyone'.
+
+
+#Create an acronym or an abbreviation for the name 'Coding For All'.
+
+
+#Use index to determine the position of the first occurrence of C in Coding For All.
+
+
+#Use index to determine the position of the first occurrence of F in Coding For All.
+
+
+#Use rfind to determine the position of the last occurrence of l in Coding For All People.
+
+
+#Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+#Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+#Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+#Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+#Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+#Does ''Coding For All' start with a substring Coding?
+
+
+#Does 'Coding For All' end with a substring coding?
+
+
+#'   Coding For All      '  , remove the left and right trailing spaces in the given string.
+
+
+#Which one of the following variables return True when we use the method isidentifier():
+
+
+#30DaysOfPython
+
+
+#thirty_days_of_python
+
+
+#The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+
+
+#Use the new line escape sequence to separate the following sentences.
